880517af593f2151d4c59638aa176ab64f1adee0
Nusrat <nusrathaque717@gmail.com>
2026-02-18 00:33:10 +0600

    footer added

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,7 @@ const displayProduct = (products) => {
 
         const creatediv = document.createElement('div');
         creatediv.innerHTML = `
-     <div class="card bg-base-100 shadow-sm h-[450px]">
+     <div class="card bg-base-100 shadow-sm h-[400px] px-4">
   <figure class="h-[200px] p-4">
     <img
     class="h-full w-full object-contain"
@@ -22,12 +22,12 @@ const displayProduct = (products) => {
       alt="Shoes" />
   </figure>
 
-  <div class="card-actions justify-end">
-  <div class="badge badge-outline">${product.category}</div>
-  <div class="flex">
-     <i class="fa-solid fa-star"></i>
+  <div class="card-actions justify-between">
+  <div class="badge badge-soft badge-primary">${product.category}</div>
+  <div class="flex items-center">
+     <i class="fa-solid fa-star text-yellow-500"></i>
      <p>${product.rating.rate}</p>
-     <p>${product.rating.count}</p>
+     <p>(${product.rating.count})</p>
   </div>
   
     </div>
@@ -36,7 +36,11 @@ const displayProduct = (products) => {
     <h2 class="card-title text-sm line-clamp-2">
       ${product.title}
     </h2>
-    <div class="badge badge-secondary">$${product.price}</div>
+    <p class="text-xl font-bold">$${product.price}</p>
+   <div>
+      <button class="btn btn-outline btn-primary"><i class="fa-regular fa-eye"></i>Details</button>
+      <button class="btn btn-outline btn-primary"><i class="fa-solid fa-cart-shopping"></i>Add</button>
+   </div>
   </div>
 </div>
     `
